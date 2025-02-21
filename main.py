52ff24fc282a1d3a5ce20e950796878cc6446d7e
raulc0399 <raci0399@gmail.com>
2025-02-21 13:05:40 +0100

    separate generate depth and lang sam

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import os
 from models import LangSAMModel, ZoeDepthModel, DepthAnythingV2Model
 
-# Create output directory if it doesn't exist
 os.makedirs("imgs/output", exist_ok=True)
 
-# Initialize models
-langsam_model = LangSAMModel()
-# zoe needs another version of timm, use pip install timm==0.6.7
-# zoe_model = ZoeDepthModel()
-depth_anything_model = DepthAnythingV2Model()
+generate_depth = False
 
-# Process images with each model
-for img_name in ["l.png", "r.png"]:
-    langsam_model.process_image(img_name)
-    # zoe_model.process_image(img_name)
-    depth_anything_model.process_image(img_name)
+if generate_depth:
+    # zoe needs another version of timm, use pip install timm==0.6.7
+    # zoe_model = ZoeDepthModel()
+    depth_anything_model = DepthAnythingV2Model()
+
+    for img_name in ["l.png", "r.png"]:
+        # zoe_model.process_image(img_name)
+        depth_anything_model.process_image(img_name)
+else:
+    langsam_model = LangSAMModel()
+    for img_name in ["l.png", "r.png"]:
+        langsam_model.process_image(img_name)
+        
