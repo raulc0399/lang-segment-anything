8352b3ac650581e6dfa43d2165efadc1b4bc8663
raulc0399 <raci0399@gmail.com>
2025-03-14 15:33:34 +0100

    seg

---
+++ seg.py
@@ -0,0 +1,13 @@
+import os
+from PIL import Image
+import numpy as np
+from models import LangSAMModel
+import cv2
+import torch
+
+os.makedirs("imgs/output", exist_ok=True)
+
+langsam_model = LangSAMModel()
+
+for img_name in ["1.png", "2.png", "3.png"]:
+    arm_mask, hand_mask = langsam_model.process_image(img_name, return_mask=True)
